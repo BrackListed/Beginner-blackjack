8adfe96c64587c61a526c08d7459629c3c80c240
BrackListed <cameta8900@gmail.com>
2026-05-14 20:07:35 +0800

    Made the balance system tangible

--- src/assets/Header.tsx
+++ src/assets/Header.tsx
@@ -1,21 +1,23 @@
+import { useState } from "react";
 import { Buttons } from "./Buttons";
 
 export function Header(){
+  let [userBalance, setBalance] = useState(parseInt(localStorage.getItem('balance-storage') ?? "0") ?? 0)
   return(
     <div className="flex flex-col  gap-5">
       <h1 className="my-6 mx-auto text-4xl font-extrabold">SIMPLE BLACKJACK GAME</h1>
       <div id = "money-container" className="flex gap-5">
 
         <div id = "left" className="flex flex-col gap-3">
-            <Buttons>CLICK TO INCREASE BALANCE</Buttons>
+            <Buttons onClick={() => increaseBalance(100)}>CLICK TO INCREASE BALANCE</Buttons>
             <div className="flex gap-3 justify-center">
-                <Buttons>+1</Buttons>
-                <Buttons>+5</Buttons>
-                <Buttons>+10</Buttons>
+                <Buttons onClick={() => increaseBalance(1)}>+1</Buttons>
+                <Buttons onClick={() => increaseBalance(5)}>+5</Buttons>
+                <Buttons onClick={() => increaseBalance(10)}>+10</Buttons>
             </div>
         </div>
         <div id = "right" className="flex flex-col gap-2">
-            <span>Balance: </span>
+            <span>Balance: {userBalance} </span>
             <label htmlFor = "bet-input">Bet: 
                 <input id = "bet-input" type = "text" className="mx-2 outline-none focus-visible:ring-1 focus-visible:ring-amber-300 w-44 rounded-lg gap-2 bg-black/40 px-2 py-1 hover:cursor-pointer transition-transform hover:scale-105"></input>
             </label>
@@ -25,4 +27,13 @@ export function Header(){
       </div>
     </div>
   )
+
+  function increaseBalance(bal: number){
+    setBalance((oldBalance) => {
+      const newBalance = oldBalance += bal
+      localStorage.setItem("balance=-storage", JSON.stringify(newBalance))
+      return newBalance
+    })
+  }
+
 }
